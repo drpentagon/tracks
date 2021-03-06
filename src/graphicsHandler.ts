31d7373de471d48be25e28fb334065a3ab42edf1
Martin Svärd <martinsvard@me.com>
2021-03-06 12:20:48 +0100

    Added graphics handler

---
+++ src/graphicsHandler.ts
@@ -0,0 +1,20 @@
+export default class GraphicsHandler {
+  canvas: HTMLCanvasElement;
+  ctx: CanvasRenderingContext2D;
+
+  constructor(parentNode: HTMLElement) {
+    const width = parentNode.offsetWidth;
+    const height = parentNode.offsetHeight;
+
+    this.canvas = document.createElement("canvas");
+    this.canvas.className = "canvas";
+    this.canvas.setAttribute("width", `${width}`);
+    this.canvas.setAttribute("height", `${height}`);
+    this.canvas.style.width = `${width}px`;
+    this.canvas.style.height = `${height}px`;
+
+    parentNode.appendChild(this.canvas);
+    this.ctx = this.canvas.getContext("2d");
+    this.ctx.lineWidth = 0.5;
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,15 @@
-let message: string = 'Hello World 3'
-console.log(message)
+import GraphicsHandler from "./graphicsHandler.js";
+
+const CONTAINER: HTMLElement = document.querySelector(".graphics-wrapper");
+
+class Application {
+  gh: GraphicsHandler;
+  constructor() {
+    this.gh = new GraphicsHandler(CONTAINER);
+  }
+
+  start() {}
+}
+
+const tracks: Application = new Application();
+tracks.start();
